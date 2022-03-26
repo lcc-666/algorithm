834d5a0b976d00022f71eca3fd90a8d0f9782c3a
lcc-666 <2502247952@qq.com>
2022-03-26 11:28:50 +0800

    快速排序

--- two/QuickSort.py
+++ two/QuickSort.py
@@ -1,5 +1,3 @@
-
-
 def Partition(a, s, t):
     i = s
     j = t
@@ -14,19 +12,16 @@ def Partition(a, s, t):
     a[i] = tmp
     return i
 
-def QuickSort(a,s, t):
+
+def QuickSort(a, s, t):
     if s < t:
         i = Partition(a, s, t)
         QuickSort(a, s, i - 1)
         QuickSort(a, i + 1, t)
 
 
-
 if __name__ == '__main__':
     a = [6, 8, 6, 4, 7, 6, 1, 56, 8]
     print("排序前", a)
-    QuickSort(a,0,len(a)-1)
-    print("排序后",a)
-
-
-
+    QuickSort(a, 0, len(a) - 1)
+    print("排序后", a)
