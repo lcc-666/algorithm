c0440a826b426a3df874ea9c11ffad9de3a7777a
lcc-666 <2502247952@qq.com>
2022-04-30 15:50:48 +0800

    最长公共子串

--- seven/son_list.py
+++ seven/son_list.py
@@ -1,47 +1,43 @@
 class long_list:
     def __init__(self):
-        MAX=10
+        MAX = 10
         dp = []
         for i in range(MAX):
             dp.append([0] * MAX)
-        subs=[]
-        a =["a","b","c","b","d","b"]
-        m=len(a)
-        b=["a","c","b","b",'a',"b","d","b","b"]
-        n=len(b)
-
+        subs = []
+        a = ["a", "b", "c", "b", "d", "b"]
+        m = len(a)
+        b = ["a", "c", "b", "b", 'a', "b", "d", "b", "b"]
+        n = len(b)
 
         def LCSlength():
-            for i in range(1,m+1):
-                for j in range(1,n+1):
-                    if a[i-1]==b[j-1]:
+            for i in range(1, m + 1):
+                for j in range(1, n + 1):
+                    if a[i - 1] == b[j - 1]:
                         dp[i][j] = dp[i - 1][j - 1] + 1
                     else:
-                        dp[i][j] = max(dp[i][j - 1],dp[i - 1][j])
+                        dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+
         def Buildsubs():
-            k=dp[m][n]
-            i=m
-            j=n
-            while k>0:
-                if dp[i][j]==dp[i-1][j]:
-                    i-=1
-                elif dp[i][j]==dp[i][j-1]:
-                    j-=1
+            k = dp[m][n]
+            i = m
+            j = n
+            while k > 0:
+                if dp[i][j] == dp[i - 1][j]:
+                    i -= 1
+                elif dp[i][j] == dp[i][j - 1]:
+                    j -= 1
                 else:
-                    subs.append(a[i-1])
-                    i-=1
-                    j-=1
-                    k-=1
+                    subs.append(a[i - 1])
+                    i -= 1
+                    j -= 1
+                    k -= 1
+
         LCSlength()
         Buildsubs()
         subs.reverse()
         print("结果LCS={}".format("".join(subs)))
+
+
 if __name__ == '__main__':
-    s=long_list()
-                        
-
-
-        
-
-
-
+    s = long_list()
